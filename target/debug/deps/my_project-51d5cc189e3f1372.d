aad21c614c3b1a1e894f18232cfd391890f853ca
sevenseasofbri <57522870+sevenseasofbri@users.noreply.github.com>
2023-12-15 07:57:30 +0000

    Add Freestanding Rust Binary

---
+++ target/debug/deps/my_project-51d5cc189e3f1372.d
@@ -0,0 +1,5 @@
+/home/runner/RustOS/target/debug/deps/libmy_project-51d5cc189e3f1372.rmeta: src/main.rs
+
+/home/runner/RustOS/target/debug/deps/my_project-51d5cc189e3f1372.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/my_project-b2bd433b563b4ffe.d
@@ -0,0 +1,5 @@
+/home/runner/RustOS/target/debug/deps/libmy_project-b2bd433b563b4ffe.rmeta: src/main.rs
+
+/home/runner/RustOS/target/debug/deps/my_project-b2bd433b563b4ffe.d: src/main.rs
+
+src/main.rs:
